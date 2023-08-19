02b0a0b0346c947504037cd3e16de0f6659681ee
Nejra Dedović <ndedovic2@etf.unsa.ba>
2023-08-19 23:14:58 +0200

    Initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+/**
+ * Main class for working with JavaFX framework
+ */
+
+public class AppFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        stage.setTitle("Pekara Zlatni dukat");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+}
---
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+}
