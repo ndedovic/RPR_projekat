224ae3ce69978b6e8aee388354fe0de2487b9dfd
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-26 16:21:47 +0200

    Napravljena pocetna stranica

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -16,7 +16,7 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class AppFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/pocetna.fxml"));
         stage.setTitle("Pekara Zlatni dukat");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setResizable(false);
