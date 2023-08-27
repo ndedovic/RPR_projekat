338200789c1be1877b5871d99dc213a92fba02ee
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-28 00:09:36 +0200

    Kreirana login stranica i odgovarajući kontroler

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    public Button loginBtn;
+    public TextField emailFld;
+    public PasswordField passwordFld;
+
+    public void otvaranjeHomePage(ActionEvent actionEvent) {
+    }
+}
