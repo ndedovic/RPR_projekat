d3aa06a7e2987dab66c0480d4079516aa75f48d6
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-27 23:06:05 +0200

    Kreiran kontroler za pocetnu stranicu

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class PocetnaController {
+    public Button aboutBtn;
+    public Button helpBtn;
+    public Button loginBtn;
+    public Button registrationBtn;
+
+    public void prikazOnama(ActionEvent actionEvent) {
+    }
+
+    public void prikazPomoci(ActionEvent actionEvent) {
+    }
+
+    public void prikazLogina(ActionEvent actionEvent) {
+    }
+
+    public void prikazRegistracije(ActionEvent actionEvent) {
+    }
+}
