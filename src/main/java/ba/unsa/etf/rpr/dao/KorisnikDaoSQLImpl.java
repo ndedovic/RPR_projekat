9e267ddce0678236d417b9f1671fb3d5e758d925
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-29 00:49:18 +0200

    Implementacija klase KorisnikDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/KorisnikDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KorisnikDaoSQLImpl.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr.dao;
 
-public class KorisnikDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.Korisnik;
+import ba.unsa.etf.rpr.exceptions.PekaraException;
+
+import java.sql.ResultSet;
+import java.util.Map;
+
+public class KorisnikDaoSQLImpl extends AbstractDao<Korisnik> implements KorisnikDao{
+    private static  KorisnikDaoSQLImpl instance = null;
+
+    public KorisnikDaoSQLImpl() {
+        super("Korisnik");
+    }
+    public static KorisnikDaoSQLImpl getInstance(){
+        if(instance == null)
+            instance = new KorisnikDaoSQLImpl();
+        return instance;
+    }
+
+    public static void removeInstance(){
+        if(instance != null)
+            instance = null;
+    }
+
+    @Override
+    public Korisnik row2object(ResultSet rs) throws PekaraException {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> object2row(Korisnik object) {
+        return null;
+    }
 }
