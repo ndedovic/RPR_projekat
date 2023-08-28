79d32d6ca8f6f69059c3e04268a8e5bd6fdcabbc
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-29 01:02:51 +0200

    Implementacija NarudzbaDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/NarudzbaDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/NarudzbaDaoSQLImpl.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr.dao;
 
-public class NarudzbaDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.Narudzba;
+import ba.unsa.etf.rpr.exceptions.PekaraException;
+
+import java.sql.ResultSet;
+import java.util.Map;
+
+public class NarudzbaDaoSQLImpl extends AbstractDao<Narudzba> implements NarudzbaDao {
+    private static NarudzbaDaoSQLImpl instance = null;
+    public NarudzbaDaoSQLImpl() {
+        super("Narudzba");
+    }
+    public static NarudzbaDaoSQLImpl getInstance(){
+        if (instance == null)
+            instance = new NarudzbaDaoSQLImpl();
+        return instance;
+    }
+    public static void removeInstance(){
+        if (instance != null)
+            instance = null;
+    }
+    @Override
+    public Narudzba row2object(ResultSet rs) throws PekaraException {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> object2row(Narudzba object) {
+        return null;
+    }
 }
