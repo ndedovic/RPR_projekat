c2e2be119e1f4d018519f12a0c6600b082826176
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-29 23:35:31 +0200

    Implementirano otvaranje login stranice klikom na dugme

--- src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
@@ -1,7 +1,20 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.AppFX;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class PocetnaController {
     public Button aboutBtn;
@@ -16,8 +29,19 @@ public class PocetnaController {
     }
 
     public void prikazLogina(ActionEvent actionEvent) {
+        try {
+            Stage stage1 = (Stage)loginBtn.getScene().getWindow();
+            stage1.close();
+            Parent newRoot = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Prijava");
+            Scene scene = new Scene(newRoot, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public void prikazRegistracije(ActionEvent actionEvent) {
-    }
 }
