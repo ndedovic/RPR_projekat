9162b4be1dc71f5c9ecb3fc9b3e1276c63d63855
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 01:07:06 +0200

    Kreiran kontroler za pomoc

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/PomocController.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class PomocController {
+    public Button zatvoriBtn;
+
+    public void zatvaranjeOnama(ActionEvent actionEvent) {
+    }
+}
