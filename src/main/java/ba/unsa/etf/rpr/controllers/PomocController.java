cfa4cc6f4237ea5007631d70099f33dd1c7bf18a
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 01:09:32 +0200

    Omoguceno zatvaranje stranice za pomoc

--- src/main/java/ba/unsa/etf/rpr/controllers/PomocController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PomocController.java
@@ -2,10 +2,13 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class PomocController {
     public Button zatvoriBtn;
 
     public void zatvaranjeOnama(ActionEvent actionEvent) {
+        Stage stage =(Stage)zatvoriBtn.getScene().getWindow();
+        stage.close();
     }
 }
