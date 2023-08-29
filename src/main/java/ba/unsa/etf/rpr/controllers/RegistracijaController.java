724a8887cee60ddc5c8e170fbc637f6c8fa0d4dc
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 00:06:56 +0200

    Kreiran kontroler za registraciju

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistracijaController.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class RegistracijaController {
+    public Button registracijaBtn;
+
+    public void akcijaRegistrovanja(ActionEvent actionEvent) {
+    }
+}
