d30ceaf6f94b59c7d4b0a28cab231addd70d7056
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 01:23:35 +0200

    Implementirana klasa DaoFactory

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Narudzba;
+
 public class DaoFactory {
+    private DaoFactory(){}
+
+    private static final ProizvodiDao proizvodiDao = ProizvodiDaoSQLImpl.getInstance();
+    public static ProizvodiDao proizvodiDao(){return proizvodiDao;}
+
+    private static final NarudzbaDao narudzbaDao = NarudzbaDaoSQLImpl.getInstance();
+    public static NarudzbaDao narudzbaDao(){return narudzbaDao;}
+
+    private static final KategorijaDao kategorijaDao = KategorijaDaoSQLImpl.getInstance();
+    public static KategorijaDao kategorijaDao(){return kategorijaDao;}
+
+    private static final KorisnikDao korisnikDao = KorisnikDaoSQLImpl.getInstance();
+    public static KorisnikDao korisnikDao(){return korisnikDao;}
 }
