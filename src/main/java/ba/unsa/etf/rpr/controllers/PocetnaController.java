9ea7fa94768ff5bec49dc93b6baa1d0e1e0102e2
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 00:22:58 +0200

    Implementirano otvaranje stranice za registraciju

--- src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PocetnaController.java
@@ -44,4 +44,19 @@ public class PocetnaController {
         }
     }
 
+    public void prikazRegistracije(ActionEvent actionEvent) {
+        try {
+            Stage stage1 = (Stage)loginBtn.getScene().getWindow();
+            stage1.close();
+            Parent newRoot = FXMLLoader.load(getClass().getResource("/fxml/registracija.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Registracija");
+            Scene scene = new Scene(newRoot, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
