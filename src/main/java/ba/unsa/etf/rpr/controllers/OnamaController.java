7d85091274022cd6b0343be0bb20480aea72a792
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 20:54:56 +0200

    JavaDoc dokumentacija za onama

--- src/main/java/ba/unsa/etf/rpr/controllers/OnamaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/OnamaController.java
@@ -4,9 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+/**
+ * kontroler za onama
+ */
 public class OnamaController {
     public Button zatvoriBtn;
 
+    /**
+     * Zatvaranje stranice o nama
+     * @param actionEvent
+     */
     public void zatvaranjeOnama(ActionEvent actionEvent) {
         Stage stage =(Stage)zatvoriBtn.getScene().getWindow();
         stage.close();
