c8dc8b0aafa5fc34ea1b2f3d1630e186e4e6fe72
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 20:56:52 +0200

    JavaDoc dokumentacija za pomoc

--- src/main/java/ba/unsa/etf/rpr/controllers/PomocController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PomocController.java
@@ -4,9 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+/**
+ * kontroler za pomoc
+ */
 public class PomocController {
     public Button zatvoriBtn;
 
+    /**
+     * Metoda za zatvaranje prozora za pomoc
+     * @param actionEvent
+     */
     public void zatvaranjeOnama(ActionEvent actionEvent) {
         Stage stage =(Stage)zatvoriBtn.getScene().getWindow();
         stage.close();
