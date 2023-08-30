e0d369a7b1c69c74f955e3243d8f18415f0e0582
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 21:38:43 +0200

    Dodana ikonica za pocetnu stranicu

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
@@ -24,6 +25,8 @@ public class AppFX extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/pocetna.fxml"));
         stage.setTitle("Pekara Zlatni dukat");
+        Image icon = new Image(getClass().getResourceAsStream("/images/ikonica.jpg"));
+        stage.getIcons().add(icon);
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setResizable(false);
         stage.show();
