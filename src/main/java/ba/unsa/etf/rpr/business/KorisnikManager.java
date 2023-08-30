918b20a10812055db4893ee53dc807ea5bad87ee
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 02:06:48 +0200

    Implementiran KorisnikManager

--- src/main/java/ba/unsa/etf/rpr/business/KorisnikManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/KorisnikManager.java
@@ -6,11 +6,27 @@ import ba.unsa.etf.rpr.exceptions.PekaraException;
 
 import java.util.List;
 
+/**
+ * Sloj biznis logike za upravljanje korisnicima
+ */
 public class KorisnikManager {
+    public void obrisiKorisnika(int id) throws PekaraException {
+        DaoFactory.korisnikDao().delete(id);
+    }
+    public void dodajKorisnika(Korisnik korisnik) throws PekaraException {
+        DaoFactory.korisnikDao().add(korisnik);
+    }
+    public Korisnik pronadjiKorisnikaPoID(int id) throws PekaraException {
+        return DaoFactory.korisnikDao().getById(id);
+    }
+    public void azurirajKorisnika(Korisnik korisnik) throws PekaraException {
+        DaoFactory.korisnikDao().update(korisnik);
+    }
+    public List<Korisnik> dajSveKorisnike() throws PekaraException {
+        return DaoFactory.korisnikDao().getAll();
+    }
     public static List<Korisnik> pronadjiKorisnikaPoEmailu(String email) throws PekaraException {
         return DaoFactory.korisnikDao().pronadjiKorisnikaPoEmailu(email);
     }
-    public static List<Korisnik> pronadjiKorisnikaPoID(int id) throws PekaraException {
-        return DaoFactory.korisnikDao().pronadjiKorisnikaPoID(id);
-    }
+
 }
