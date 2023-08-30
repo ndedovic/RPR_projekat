beabaed51e819c4565abb5cd13427ed3aba28492
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 19:17:43 +0200

    Omogućeno potvrđivanje narudzbe

--- src/main/java/ba/unsa/etf/rpr/controllers/KorpaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/KorpaController.java
@@ -2,8 +2,14 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class KorpaController {
     public Label nazivLbl;
@@ -20,6 +26,19 @@ public class KorpaController {
     }
 
     public void potvrdiNarudzbu(ActionEvent actionEvent) {
+        try {
+            Stage stage1 = (Stage) potvrdiID.getScene().getWindow();
+            stage1.close();
+            Parent newRoot = FXMLLoader.load(getClass().getResource("/fxml/potvrdaNarudzbe.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Potvrda");
+            Scene scene = new Scene(newRoot, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e1) {
+            System.out.println(e1.getMessage());
+        }
     }
 
     public void otkaziNarudzbu(ActionEvent actionEvent) {
