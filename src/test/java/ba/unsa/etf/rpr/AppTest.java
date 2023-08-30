19f60404319d227af83975e23f9fc14559131bb7
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 21:22:40 +0200

    Uređen četvrti test

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -3,8 +3,16 @@ package ba.unsa.etf.rpr;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import ba.unsa.etf.rpr.business.KategorijaManager;
+import ba.unsa.etf.rpr.business.KorisnikManager;
+import ba.unsa.etf.rpr.domain.Kategorija;
 import ba.unsa.etf.rpr.domain.Korisnik;
+import ba.unsa.etf.rpr.domain.Proizvodi;
+import ba.unsa.etf.rpr.exceptions.PekaraException;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -24,5 +32,20 @@ public class AppTest
     @Test
     public void Test2(){
         assertEquals(8, k1.getPassword().length());
+    }
+    Kategorija kategorija1 = new Kategorija(1,"slatko");
+    Kategorija kategorija2 = new Kategorija(2, "slano");
+    Proizvodi p = new Proizvodi(1, "hljeb", "2", 2);
+
+    @Test
+    public void Test3(){
+        assertTrue(p.getKategorija()==kategorija2.getId());
+    }
+    Korisnik k2 = new Korisnik(11, "Meho", "Mehic", "meho@gmail.com", "11111111");
+
+    @Test
+    public void Test4() throws PekaraException {
+        KorisnikManager manager = new KorisnikManager();
+        assertTrue(manager.dajSveKorisnike().contains(k2));
     }
 }
