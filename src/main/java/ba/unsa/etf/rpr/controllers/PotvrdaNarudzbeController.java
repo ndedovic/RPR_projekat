aa2546069da46a437e731c58750bda90f0413a0d
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 19:10:54 +0200

    Kreiran fxml i kontroler za potvrdu narudzbe

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/PotvrdaNarudzbeController.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class PotvrdaNarudzbeController {
+    public Button zatvoriBtn;
+
+    public void zatvoriProzor(ActionEvent actionEvent) {
+    }
+}
