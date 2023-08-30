12529c36c6ef5374dc808f681935c2d96f01e9e3
ndedovic <ndedovic2@etf.unsa.ba>
2023-08-30 16:14:09 +0200

    Napravljena stranica za shop

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProdavnicaController.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class ProdavnicaController {
+    public Button btnKorpa;
+    public Button btnSlatko;
+    public Button btnSlano;
+    public Button btnDodajUKorpu;
+
+    public void prikaziKorpu(ActionEvent actionEvent) {
+    }
+
+    public void prikaziSlatkeProizvode(ActionEvent actionEvent) {
+    }
+
+    public void prikaziSlaneProizvode(ActionEvent actionEvent) {
+    }
+
+    public void dodajUProizvodUKorpu(ActionEvent actionEvent) {
+    }
+}
